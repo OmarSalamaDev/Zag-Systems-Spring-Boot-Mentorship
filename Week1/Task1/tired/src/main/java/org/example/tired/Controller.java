9c93c8bd5340dfe492c63f428f074ff056bb31bf
Mohamed Adel <mohamedadel96e@gmail.com>
2024-12-04 19:09:17 +0200

    Solve Some API Problems

--- Week1/Task1/tired/src/main/java/org/example/tired/Controller.java
+++ Week1/Task1/tired/src/main/java/org/example/tired/Controller.java
@@ -1,25 +1,21 @@
 package org.example.tired;
 
-import org.example.tired.FrontEndLayer.IMPL.EmployeeService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.example.tired.IMPL.EmployeeService;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@CrossOrigin(origins = "*")
 @RestController
-@RequestMapping("/api/employees")
 public class Controller {
     private final EmployeeService employeeService;
     public Controller(EmployeeService employeeService) {
         this.employeeService = employeeService;
     }
-    @GetMapping
+    @GetMapping("/api/employees")
     public List<Employee> getAllEmployees() {
         return employeeService.getAllEmployees();
     }
-    @PostMapping
+    @PostMapping("/api/employees")
     public void addEmployee(Employee employee) {
         employeeService.addEmployee(employee);
     }
---
+++ Week1/Task1/tired/src/main/java/org/example/tired/IMPL/EmployeeService.java
@@ -0,0 +1,22 @@
+package org.example.tired.IMPL;
+
+import org.example.tired.Employee;
+import org.example.tired.EmployeeDAO;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeService {
+    private final EmployeeDAO employeeDAO;
+    EmployeeService(EmployeeDAO employeeDAO) {
+        this.employeeDAO = employeeDAO;
+    }
+
+    public List<Employee> getAllEmployees() {
+        return employeeDAO.getAllEmployees();
+    }
+    public void addEmployee(Employee employee) {
+        employeeDAO.addEmployee(employee);
+    }
+}
